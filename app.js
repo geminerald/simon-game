fcab4187ff5879f2bc2f5416a320f8f9eb8afefb
geminerald <zacdwarner@gmail.com>
2019-09-03 23:37:36 +0100

    added html elements for buttons, colours and basic CSS, selected colour buttons in JS and rough outline of project

---
+++ app.js
@@ -0,0 +1,15 @@
+// Colour Selectors
+
+const red = document.getElementById("red");
+const yellow = document.getElementById("yellow");
+const green = document.getElementById("green");
+const blue = document.getElementById("blue");
+
+// Arrays
+
+const gameArray = [];
+const userArray = [];
+
+// Change Colour Function
+
+// Random Number Generator and push to array
