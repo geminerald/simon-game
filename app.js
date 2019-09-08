aeab35b8d75c9d1e2ffec07bd7cb9bf242a9ddaa
geminerald <zacdwarner@gmail.com>
2019-09-08 17:11:44 +0100

    updated colours, removed inline script test from index, added button click to change colour options

--- app.js
+++ app.js
@@ -17,7 +17,35 @@ const userArray = [];
 
 function beClicked(colour){
     readout.innerHTML = (`${colour.id} button is clicked`);
-    console.log(colour.id);
+
+    var color = getComputedStyle(colour).backgroundColor;
+    
+    var rgbToHex = function (rgb) { 
+        var hex = Number(rgb).toString(16);
+        if (hex.length < 2) {
+             hex = "0" + hex;
+        }
+        return hex;
+      };
+    
+    var fullColorHex = function(r,g,b) {   
+        var red = rgbToHex(r);
+        var green = rgbToHex(g);
+        var blue = rgbToHex(b);
+        return red+green+blue;
+      };
+
+     // colour.style.backgroundColor = fullColorHex(color) + 500;
+      var testColour = (250, 150, 05);
+      console.log(fullColorHex(testColour));
+
+   // colour.style.backgroundColor = `${getComputedStyle(colour).backgroundColor} + 100`
+
+
+
+    //console.log(getComputedStyle(colour).backgroundColor);
+   // console.log(colour.id);
+    
 };
 
 red.addEventListener('click',()=>{
@@ -37,4 +65,9 @@ blue.addEventListener('click',()=>{
 }); 
 
 // Random Number Generator and push to array
+
+
+
+
+
 
